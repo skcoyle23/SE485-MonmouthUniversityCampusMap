636531219111fd6cd371cf8ae68a649ac475c25e
Shannon Coyle <52424103+skcoyle23@users.noreply.github.com>
2022-01-18 14:27:13 -0500

    Add files via upload
    Initial commit of the welcome screen for the map

---
+++ CampusMap-WelcomeScreen/CampusMap-WelcomeScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampusMap-WelcomeScreen
+//
+//  Created by Shannon Coyle on 1/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CampusMap-WelcomeScreen/CampusMap-WelcomeScreenUITests/CampusMap_WelcomeScreenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CampusMap_WelcomeScreenUITestsLaunchTests.swift
+//  CampusMap-WelcomeScreenUITests
+//
+//  Created by Shannon Coyle on 1/7/22.
+//
+
+import XCTest
+
+class CampusMap_WelcomeScreenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
